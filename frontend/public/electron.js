2f67fbebd667a4fbf2f21d7bc49b9917a74b958a
adurmusoglu <durmusoglu555@gmail.com>
2026-02-04 18:47:00 -0600

    debugging, react works in electron

--- frontend/public/electron.js
+++ frontend/public/electron.js
@@ -11,7 +11,7 @@ function createWindow() {
     }
   });
 
-  win.loadFile(path.join(__dirname, 'index.html'));
+  win.loadURL('http://localhost:3000');
 }
 
 app.whenReady().then(createWindow);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+console.log("index.js is running");
+console.log("Looking for root element:", document.getElementById("root"));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log("About to render App component");
 root.render(<App />);

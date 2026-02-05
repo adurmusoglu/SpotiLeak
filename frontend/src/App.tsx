d8462e219fadb9b64524c4627433b19fe8301082
adurmusoglu <durmusoglu555@gmail.com>
2026-02-04 22:42:23 -0600

    switched to typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+function App(): React.JSX.Element {
   console.log("App component is rendering");
   return (
-    <div>
+    <div> 
       <h1>SpotiLeak</h1>
         <p>Ever heard a song on social media that wasn't on Spotify?
         Listen to all the songs you love with SpotiLeak! </p>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 
 console.log("index.js is running");
 console.log("Looking for root element:", document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 console.log("About to render App component");
 root.render(<App />);
